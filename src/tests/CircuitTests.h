bf565eb4018d7c7436540a19b040a2ad44fa4708
Michalek007 <nmichal02@op.pl>
2023-03-11 00:55:46 +0100

    Modified: project structure

---
+++ src/tests/CircuitTests.h
@@ -0,0 +1,22 @@
+//
+// Created by Michał on 06.01.2023.
+//
+
+#ifndef CIRCUITSIMULATOR_CIRCUITTESTS_H
+#define CIRCUITSIMULATOR_CIRCUITTESTS_H
+
+#include "Circuit.h"
+
+class CircuitTests {
+public:
+    static bool float_comparison(float x, float y);
+    static void circuit_test_1();
+    static void circuit_test_2();
+    static void circuit_test_3();
+    static void circuit_test_4();
+    static void circuit_test_5();
+
+};
+
+
+#endif //CIRCUITSIMULATOR_CIRCUITTESTS_H
